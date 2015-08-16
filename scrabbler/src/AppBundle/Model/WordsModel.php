09f295d2f4ab85fc8d05248e0ec90952a37a17eb
JimmehWhy <james-y@live.co.uk>
2015-08-16 21:52:38 +0100

    words model crappy implementation

--- scrabbler/src/AppBundle/Model/WordsModel.php
+++ scrabbler/src/AppBundle/Model/WordsModel.php
@@ -21,8 +21,35 @@ class WordsModel {
         //var_dump("In model");
         $this->letters = $letters;
 
-        $this->words = array('and', 'an', 'am', 'dam', 'mad');
+        $chars = str_split($letters);
+        //var_dump($chars);
+
+        $this->words = $this->permutations($chars, count($chars) -1);
+        //var_dump($this->words);
 
         return $this->words;
+    }
+
+    function permutations($chars, $size, $combinations = array()){
+        if(empty($combinations)){
+            $combinations = $chars;
+        }
+
+        if($size == 1){
+            return $combinations;
+        }
+
+        $new_combinations = array();
+
+        # loop through existing combinations and character set to create strings
+        foreach ($combinations as $combination) {
+            foreach ($chars as $char) {
+                //TODO needs a braking mechanism
+                $new_combinations[] = $combination . $char;
+            }
+        }
+
+        # call same function again for the next iteration
+        return $this->permutations($chars, $size - 1, $new_combinations);
     }
 }
